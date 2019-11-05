de3a2de1b1d7b703212a3667e7911ac219c60504
Tony Mowers <tony@amowers.com>
2019-11-05 11:29:35 +0100

    #12 add Swagger API annotations to User REST API

--- rest-with-spring/src/main/java/com/alika/examples/rest/SwaggerConfig.java
+++ rest-with-spring/src/main/java/com/alika/examples/rest/SwaggerConfig.java
@@ -8,6 +8,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
@@ -23,10 +27,14 @@ public class SwaggerConfig {
             CONTACT,
             "Apache 2.0",
             "http://www.apache.org/licenses/LICENSE-2.0");
+    private static final Set<String> PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList(
+            "application/json","application/xml"));
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(DEFAULT_API_INFO);
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(PRODUCES_AND_CONSUMES)
+                .consumes(PRODUCES_AND_CONSUMES);
     }
 }
--- rest-with-spring/src/main/java/com/alika/examples/rest/user/User.java
+++ rest-with-spring/src/main/java/com/alika/examples/rest/user/User.java
@@ -1,8 +1,9 @@
 package com.alika.examples.rest.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Value;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -10,12 +11,15 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@ApiModel(description = "An entity representing a person who can own resources")
 class User {
     private Integer id;
 
+    @ApiModelProperty(notes="Name must have at least two characters")
     @Size(min = 2, message = "User name must have at least two characters")
     private String name;
 
+    @ApiModelProperty(notes="Birth date must be in the past")
     @Past( message = "User birthDate must be in the past")
     private Date birthDate;
 }
