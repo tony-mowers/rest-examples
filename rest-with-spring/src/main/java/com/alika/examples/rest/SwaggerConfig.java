0e8eed6bb802b182cf87ee63f1e7d027dca01980
Tony Mowers <tony@amowers.com>
2019-11-05 11:10:07 +0100

    #11 override default Swagger ApiInfo at /v2/api-docs
    revert Swagger library from 2.9.2 to 2.4.0 so Json renders at Json in browser rather than XML

--- rest-with-spring/src/main/java/com/alika/examples/rest/SwaggerConfig.java
+++ rest-with-spring/src/main/java/com/alika/examples/rest/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.alika.examples.rest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -9,8 +11,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    private static final Contact CONTACT = new Contact(
+            "Tony the Programmer",
+            "https://www.tonytheprogrammer.com",
+            "");
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "AWESOME API Title",
+            "AWESOME Api Description",
+            "1.0",
+            "urn:tos",
+            CONTACT,
+            "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0");
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO);
     }
 }
